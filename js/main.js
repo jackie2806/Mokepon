367466de1ef0ad7196c2bb8be094d591fbeb293a
jackie2806 <jaishackeline@gmail.com>
2023-01-09 11:53:37 -0500

    Verificar si existe una mascota seleccionada en Js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
 
-function iniciarJuego(){    
-    let botonMascotaJugador = document.getElementById('boton-mascota')  
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador) 
+    
 }
 
-function seleccionarMascotaJugador(){
-    alert('SELECCIONASTE TU MASCOTA')
+function seleccionarMascotaJugador() {
+    hipodoge = document.getElementById('hipodoge')
+    capipepo = document.getElementById('capipepo')
+    ratigueya = document.getElementById('ratigueya')
+    if(hipodoge.checked === true){
+        alert('SELECCIONASTE: '+ hipodoge.id)
+    } else if (capipepo.checked === true){
+        alert('SELECCIONASTE: '+ capipepo.id)
+    } else if (ratigueya.checked === true){
+        alert('SELECCIONASTE: '+ ratigueya.id)
+    } else {
+        alert('SELECCIONA UNA MASCOTA')
+    }
+    
 }
-//La etiqueta script que está en la etiqueta <head></head> del HTML necesita que primero se cargue 
+
+//La etiqueta SCRIPT que está en la etiqueta HEAD del HTML necesita que primero se cargue 
 window.addEventListener('load', iniciarJuego)
+
