de0fc717bdc36aed896512c890b73232919f9dd5
jackie2806 <jaishackeline@gmail.com>
2023-01-23 17:28:41 -0500

    función enemigo

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
 
 function iniciarJuego() {
-    let botonMascotaJugador = document.getElementById('boton-mascota')
-    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+    let botonMascotaJugador = document.getElementById('boton-mascota');
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador);
     
 }
 
 function seleccionarMascotaJugador() {
-    let inputHipodoge = document.getElementById('hipodoge')
-    let inputCapipepo = document.getElementById('capipepo')
-    let inputRatigueya = document.getElementById('ratigueya')
-    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+    let spanMascotaJugador = document.getElementById('mascota-jugador');
     
     if(inputHipodoge.checked){
         spanMascotaJugador.innerHTML= 'Hipodoge';
         
-    } else if (inputCapipepo.checked === true){
-        spanMascotaJugador.innerHTML= 'Capipepo'
-    } else if (inputRatigueya.checked === true){
-        spanMascotaJugador.innerHTML= 'Ratigueya'
+    } else if (inputCapipepo.checked){
+        spanMascotaJugador.innerHTML= 'Capipepo';
+    } else if (inputRatigueya.checked){
+        spanMascotaJugador.innerHTML= 'Ratigueya';
     } else {
-        alert('SELECCIONA UNA MASCOTA')
+        alert('SELECCIONA UNA MASCOTA');
     }
-    
+    seleccionarMascotaEnemigo();
+
 }
+function seleccionarMascotaEnemigo(){
+    let ataqueEnemigoAleatorio = aleatorio(1,3);
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo');
+
+    if(ataqueEnemigoAleatorio == 1){
+        spanMascotaEnemigo.innerHTML= 'Hipodoge';
+        
+    } else if (ataqueEnemigoAleatorio == 2){
+        spanMascotaEnemigo.innerHTML= 'Capipepo';
+    } else {
+        spanMascotaEnemigo.innerHTML= 'Ratigueya';
+    }
+         
+
+}
+
 function aleatorio(min, max){
     return Math.floor(Math.random()*(max-min +1)+min)
 
 }
 
+
 //La etiqueta SCRIPT que está en la etiqueta HEAD del HTML necesita que primero se cargue 
-window.addEventListener('load', iniciarJuego)
+window.addEventListener('load', iniciarJuego);
 
